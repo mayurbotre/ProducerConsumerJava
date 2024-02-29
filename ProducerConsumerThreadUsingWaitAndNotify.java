c7adb4768a4806dc19769383d0b3fbb2a04b4f9c
mayurbotre <mayurbotre95@gmail.com>
2024-02-29 22:12:57 +0530

    First commit

---
+++ ProducerConsumerThreadUsingWaitAndNotify.java
@@ -0,0 +1,13 @@
+package MultiThreading;
+
+public class ProducerConsumerThreadUsingWaitAndNotify {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        
+
+
+
+    }
+
+}
