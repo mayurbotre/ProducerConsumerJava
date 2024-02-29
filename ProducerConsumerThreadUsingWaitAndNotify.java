df0c30b4a1b689a1f3c63b47cd4acb5d90300ab5
mayurbotre <mayurbotre95@gmail.com>
2024-02-29 22:24:42 +0530

    Added producer and consumer definition

--- ProducerConsumerThreadUsingWaitAndNotify.java
+++ ProducerConsumerThreadUsingWaitAndNotify.java
@@ -1,13 +1,61 @@
 package MultiThreading;
 
+import java.util.LinkedList;
+
 public class ProducerConsumerThreadUsingWaitAndNotify {
 
-    public static void main(String[] args) throws InterruptedException {
 
-        
+    public static class ProducerConsumer {
 
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        int capacity = 2;
+
+        public void produce() throws InterruptedException{
+
+            int value = 0;
+
+            while(true){
+                synchronized (this){
+                    while(linkedList.size()==capacity){
+                        wait();
+                    }
+                    System.out.println("Producer Produced - "+value);
+
+                    linkedList.add(value++);
+
+                    notify();
+
+                    Thread.sleep(1000);
+
+                }
+
+            }
+
+        }
+
+        public void consume() throws InterruptedException {
+
+            while(true){
+                synchronized (this){
+
+                    while(linkedList.size()==0){
+                        wait();
+                    }
+
+                    int val = linkedList.removeFirst();
+
+                    System.out.println("Consumer Consumed - "+ val);
+
+                    notify();
+
+                    Thread.sleep(1000);
+
+                }
+            }
+
+        }
 
 
     }
-
 }
